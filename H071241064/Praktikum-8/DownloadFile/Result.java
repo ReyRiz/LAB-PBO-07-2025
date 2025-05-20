f91df1535b916d506b9fdd9f2e1c69de865656fc
naylazaky <lovegoddestluna@gmail.com>
2025-05-20 19:04:32 +0800

    Praktikum 8

---
+++ H071241064/Praktikum-8/DownloadFile/Result.java
@@ -0,0 +1,17 @@
+package DownloadFile;
+
+public class Result {
+    int fileId;
+    String threadName;
+    int duration;
+    String status;
+
+    Result(int fileId, String threadName, int duration, String status) {
+        this.fileId = fileId;
+        this.threadName = threadName;
+        this.duration = duration;
+        this.status = status;
+    }
+}
+
+
