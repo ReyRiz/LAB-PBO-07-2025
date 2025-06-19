8e7dfaf8a04460f99c4926fc43a764ef6b47595d
Akram-Dwf <akramtamir12@gmail.com>
2025-06-19 14:31:28 +0800

    Tugas Praktikum 9_H071241076

---
+++ H071241076/Praktikum_9/Gradle1/app/src/main/java/gradle1/Post.java
@@ -0,0 +1,22 @@
+package gradle1;
+
+public class Post {
+    private String caption;
+    private String postImagePath;
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getPostImagePath() {
+        return postImagePath;
+    }
+
+    public void setPostImagePath(String postImagePath) {
+        this.postImagePath = postImagePath;
+    }
+}
---
+++ H071241076/Praktikum_9/Gradle1/app/src/main/java/gradle1/User.java
@@ -0,0 +1,31 @@
+package gradle1;
+
+public class User {
+    private String nickName;
+    private String fullName;
+    private String profileImagePath;
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getProfileImagePath() {
+        return profileImagePath;
+    }
+
+    public void setProfileImagePath(String profileImagePath) {
+        this.profileImagePath = profileImagePath;
+    }
+}
